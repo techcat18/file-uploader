8990e3846432cca4aea49882f4f532e1ab324d3c
Kostiantyn Bondarenko <kostiantyn_bondarenko1@epam.com>
2023-03-08 22:58:54 +0200

    Adds UserInfoModelValidator

---
+++ src/api/FileUploader.API/Validators/UserInfoModelValidator.cs
@@ -0,0 +1,15 @@
+﻿using FileUploader.API.Models;
+using FluentValidation;
+
+namespace FileUploader.API.Validators
+{
+    public class UserInfoModelValidator: AbstractValidator<UserInfoModel>
+    {
+        public UserInfoModelValidator()
+        {
+            RuleFor(ui => ui.Email)
+                .NotEmpty()
+                .EmailAddress();
+        }
+    }
+}
